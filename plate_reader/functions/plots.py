fac3dae8f13b5f964fb15fe13d6136e770c79610
Mialy DeFelice <mialymurie@gmail.com>
2019-01-16 18:37:52 -0800

    Added a plots.py file to handle plotting functions

---
+++ plate_reader/functions/plots.py
@@ -0,0 +1,18 @@
+def quick_plot_all_wells(df, fig_save_path):
+    wavelengths_used = df.columns.values.tolist()
+    for wavelength in wavelengths_used:
+        fig, axs = plt.subplots(8, 12, sharex='col', sharey='row', figsize = (20, 10))
+        sns.set_style('ticks')
+        #right now the axs are 2 dimensional, so need to flatten
+        #so i can iterate in a single dimension.
+        axs = axs.flatten()
+        fig.subplots_adjust(hspace = .2, wspace=.2)
+        x = convert_timestamp(list(df[wavelength].iloc[0]))
+        for i in range(2, 98):
+            y = [float(j) for j in list(df[wavelength].iloc[i])]
+            axs[i-2].plot(x, y, color = '#d55e00')
+            axs[i-2].set_ylim(0, 0.6)
+    fig.savefig(os.path.join(fig_save_path, 'plot_all_wells_' + wavelength + '.pdf'), 
+                    bbox_inches='tight')
+    plt.show()
+    return 
