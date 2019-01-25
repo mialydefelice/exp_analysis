c73abd739f23f92155aae58e760a6d8aef876aa0
Mialy DeFelice <mialymurie@gmail.com>
2019-01-25 14:40:08 -0800

    updated functions to add in ability to specify if data was to be merged and how, and add more automation

--- plate_reader/functions/data_handling.py
+++ plate_reader/functions/data_handling.py
@@ -8,7 +8,7 @@ import re
 import csv
 import seaborn as sns
 
-def find_data_path(filename, base_file_dir):
+def find_data_path(filename_base, base_file_dir):
     """
     If it is not already so, make sure the filename and the enclosing
     folder have the same name.
@@ -20,12 +20,14 @@ def find_data_path(filename, base_file_dir):
     Assumes dates are stored exactly as shown above.
 
     """
-    year_folder = '20' + filename[4:6]
-    file_folder = filename[0:filename.index('.')]
+    year_folder = '20' + filename_base[4:6]
+    #file_folder = filename[0:filename.index('.')]
     experimental_folder_path = os.path.join(base_file_dir,
-                                            year_folder, file_folder, filename)
+                                            year_folder, filename_base)
+    path_excel = os.path.join(experimental_folder_path, filename_base + '.xlsx')
+    path_data = os.path.join(experimental_folder_path, filename_base + '.txt')
 
-    return experimental_folder_path, year_folder, file_folder
+    return path_data, path_excel, year_folder, filename_base
 
 def import_data(path_to_data):
     """
@@ -154,21 +156,9 @@ def create_df_all_wavelengths(data_all_wavelengths, wavelengths_used):
     df = pd.DataFrame(index=df_index)
     for i in range(0, len(data_all_wavelengths)):
         transposed_list = list(zip(*data_all_wavelengths[i][1:]))
+        for j in range(2,len(transposed_list)):
+            transposed_list[j] = [float(i) for i in transposed_list[j]]
         df[wavelengths_used[i]] = transposed_list[0:]
-    return df
-
-def parse_data_file(filename, base_file_path):
-    """
-    Should be able to take in a file directly output from the
-    Huang lab plate reader, and parse the data and return
-    files based on the number of channels read.
-
-    Data is exported as a text file, with non ascci characters.
-    """
-    data_path = find_data_path(filename, base_file_path)
-    imported_data = import_data(data_path[0])
-    data_all_waves, waves_used = get_list_of_data(imported_data)
-    df = create_df_all_wavelengths(data_all_waves, waves_used)
     return df
 
 def to_min(s):
@@ -184,7 +174,165 @@ def convert_timestamp(ts):
 
 def create_fig_save_folder(base_file_path, file_name):
     main_output_folder_info = find_data_path(file_name, base_file_path)
-    fig_save_path = os.path.join(base_file_path, main_output_folder_info[1], main_output_folder_info[2], 'analysis_figures')
+    fig_save_path = os.path.join(base_file_path, main_output_folder_info[2], main_output_folder_info[3], 'analysis_figures')
     if not os.path.exists(fig_save_path):
         os.makedirs(fig_save_path)
     return fig_save_path
+
+def drop_excluded_wells(df, well_exclusion_list):
+    df_dropped = df.drop(well_exclusion_list)
+    return df_dropped
+
+def get_condition_details(df):
+    """
+    This function will pull out all the conditions for how the 
+    experiment was run
+    """
+    cond_details = []
+    unique_values = df['cell'].unique()
+    cond_details.append(list(filter(lambda v: v==v, unique_values)))
+    return cond_details
+
+def group_conditional_ids(df):
+    all_conditions = get_condition_details(df)[0]
+    cond_idx = []
+    for condition in all_conditions:
+        cond_df = df.loc[(df['cell'] == condition)]
+        cond_idx.append(cond_df.index.tolist())
+    col_names = get_non_cell_col_names(df)
+    return cond_idx, all_conditions, col_names
+
+def averageSingleCondition(single_group_data):
+    """
+    single_group_data should be a list of lists containing all data
+    that belongs to a single treatment group.
+    """
+    averaged_data = [sum(e)/len(e) for e in zip(*single_group_data)] 
+    return averaged_data
+
+def stdSingleCondition(single_group_data):
+    """
+    single_group_data should be a list of lists containing all data
+    that belongs to a single treatment group.
+    """
+    std_data = []
+    for e in zip(*single_group_data):
+        cntN = len(e)
+        avgVal = float(sum(e))/cntN
+        std_data.append((sum((i-avgVal)**2 for i in e)/(cntN-1))**0.5)
+    return std_data
+def get_non_cell_col_names(df):
+    col_names = df.columns.values.tolist()
+    col_names.remove('cell')
+    return col_names
+
+def create_merged_df(merged_list, column_names, well_ids, condition_names):
+    df_merged_conditions = pd.DataFrame(merged_list).T
+    df_merged_conditions.columns = column_names
+    df_merged_conditions['well_id'] = well_ids
+    df_merged_conditions['cell'] = condition_names
+    df_merged_conditions.set_index('well_id', inplace = True)
+    return df_merged_conditions    
+
+def merge_replicates(df):
+    condition_ids, condition_names, col_names = group_conditional_ids(df)
+      
+    all_data_to_merge = []
+    first_well_id = []
+    condition_name = []
+    
+    for col_name in col_names:
+        condition_data_to_merge = []
+        for condition_pair in condition_ids:
+            data_for_pairs = []
+            for well_id in condition_pair:
+                data_for_pairs.append(df.loc[well_id, col_name])
+            condition_data_to_merge.append(data_for_pairs)
+            condition_name.append(df.loc[condition_pair[0], 'cell'])
+            first_well_id.append(condition_pair[0])
+        all_data_to_merge.append(condition_data_to_merge)
+    df_merged = create_merged_df(all_data_to_merge, col_names, first_well_id, condition_name)
+    return df_merged
+
+def rework_df_bio_rep(df):
+    df['cell'] = df['cell_type'] + '_' + df['stimulus_type'] + '_' + df['Media'].map(str)
+    df.drop(['cell_type', 'cell_biol_replicate_num', 'stimulus_type', 'Media'], axis = 1, inplace=True)
+    return df
+    
+def rework_df(df):
+    df['cell'] = df['cell_type'] + '_' + df['cell_biol_replicate_num'] + '_' + df['stimulus_type'] + '_' + df['Media'].map(str)
+    df.drop(['cell_type', 'cell_biol_replicate_num', 'stimulus_type', 'Media'], axis = 1, inplace=True)
+    return df
+
+def merge_conditions(df, merge_biological_replicates, merge_tech_replicates):
+    '''
+        Input data frame that already has dropped wells that we dont want to include
+        and which already has well data associated.
+    '''
+
+    if merge_biological_replicates:
+        df = rework_df_bio_rep(df)
+        merged_df = merge_replicates(df)
+    elif merge_tech_replicates:
+        df = rework_df(df)
+        merged_df = merge_replicates(df)
+    else:
+        df = rework_df(df)
+        merged_df = df
+
+    return merged_df
+
+def average_merged_dataframe(df):
+    column_names = get_non_cell_col_names(df)
+    for col_name in column_names:
+        if type(df[col_name][0][0]) == list:
+            averaged_condition = []
+            std_condition = []
+            new_col_name_avg = col_name + '_averaged'
+            new_col_name_std = col_name + '_std'
+            for index, row in df.iterrows():
+                averaged_condition.append(averageSingleCondition(row.loc[col_name]))
+                std_condition.append(stdSingleCondition(row.loc[col_name]))
+            df[new_col_name_avg] = averaged_condition
+            df[new_col_name_std] = std_condition
+    return df
+
+def make_layout_df(filename_base, base_file_path, wells_to_exclude):
+    plate_data_path = find_data_path(filename_base, base_file_path)[1]
+    df = pd.read_excel(plate_data_path, sheetname='PlateLayout')
+    df['well_id'] = df['row']+ df['col'].map(str)
+    df.drop(['row', 'col'], axis = 1, inplace=True)
+    df.cell_biol_replicate_num = df.cell_biol_replicate_num.fillna(0).astype(np.int64)
+    df.cell_biol_replicate_num = df.cell_biol_replicate_num.astype(str)
+    df.set_index('well_id', inplace=True)
+    df_d = drop_excluded_wells(df, wells_to_exclude)
+    return df_d
+
+def make_merged_df(data_df, filename_base, base_filepath, wells_to_exclude, mer_bio_rep, mer_tec_rep):
+    df_layout = make_layout_df(filename_base, base_filepath, wells_to_exclude)
+    df_w_data = df_layout.join(data_df, how='outer')
+    merged_df = merge_conditions(df_w_data, mer_bio_rep, mer_tec_rep)
+    averaged_df = average_merged_dataframe(merged_df)
+    return averaged_df
+
+def get_time_axis(df, wavelength):
+    time = convert_timestamp(list(df[wavelength].iloc[0]))
+    return time
+
+def parse_data_file(filename, base_file_path, wells_to_exclude,
+                           merge_bio_rep, merge_tech_rep):
+    """
+    Should be able to take in a file directly output from the
+    Huang lab plate reader, and parse the data and return
+    files based on the number of channels read.
+
+    Data is exported as a text file, with non ascci characters.
+    """
+    data_path = find_data_path(filename, base_file_path)
+    imported_data = import_data(data_path[0])
+    data_all_waves, waves_used = get_list_of_data(imported_data)
+    df = create_df_all_wavelengths(data_all_waves, waves_used)
+    time = get_time_axis(df, waves_used[0])
+    df_merged = make_merged_df(df, filename, base_file_path, wells_to_exclude,
+                           merge_bio_rep, merge_tech_rep)
+    return df, df_merged, time
